21b0db05ddda95c34c4918f0ddf07aa2197d9e38
Grasit <idongesiteshiet980@gmail.com>
2020-06-03 04:34:48 +0100

    Upgrade

--- grasit.js
+++ grasit.js
@@ -1,5 +1,5 @@
 const name ="Idongesit Eshiet";
 const ID = "HNG-00368";
 const language = "JavaScript";
-const result = "Hello World, this is" + " " + name + " " + "with HNGi7 ID" + " " + ID + " " + "using" + " " + language + " " + "for stage 2 task.";
+const result = "Hello World, this is" + " " + name + " " + "with HNGi7 ID" + " " + ID + " " + "using" + " " + language + " " + "for stage 2 task";
 console.log(result);
